8c0d95fa53b12b7e431827719f2c974b7fef40bd
shubham basu <shubham basu>
2016-03-22 09:07:26 +0530

    'htmladded'

--- app/src/controllers/controller.js
+++ app/src/controllers/controller.js
@@ -1,10 +1,28 @@
-app.controller("appController",['$scope','$http',function($scope,$http){
+app.controller("appController",['$scope','$http','$location','$rootScope',function($scope,$http,$location,$rootScope){
    console.log("here");
-  
+   var config = {
+              'Content-Type':'text/plain'
+            }
+
  $scope.authenticate= function(user) {
-        $http.get("http://localhost:8181/").then(function(response) {
-       alert("hola");
-   });
+//         $http.post("http://localhost:8181/",$scope.user,config).then(function(response) {
+      
+//    });
+$rootScope.loggedInUser=$scope.user;
+    $location.path("/home");
     }
 }]);
 
+app.controller("homeCtrl",[
+    '$scope','$http','$location',function($scope,$http,$location){
+        
+        $scope.categories=["inspirational","love","family","life","war","patriotism"];
+        $scope.selectedCategory=function(category) {
+            alert(category);
+            $location.path("/quote");
+        }
+    }]);
+    
+    
+    //o global variable
+    //ng-messages validation
--- app/src/routing/router.js
+++ app/src/routing/router.js
@@ -1,14 +1,32 @@
 app.config(['$routeProvider',function($routeProvider) {
     console.log("here");
-    $routeProvider.when('/home',{
+    $routeProvider.when('/login',{
         templateUrl:'views/login.html',
         controller:''
         
+    }).when('/home',{
+        templateUrl:'views/home.html',
+        controller:'homeCtrl'
+    }).when('/quote/:categoarg',{
+        templateUrl:'views/quote.html'
     }).otherwise({
-        redirectTo:'/home'
+        redirectTo:'/login'
       
     });
-}]);
+}]).run(function($rootScope, $location) {
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      if ($rootScope.loggedInUser== null) {
+        // no logged user, redirect to /login
+        if ( next.templateUrl === "views/login.html") {
+        } else {
+          $location.path("/login");
+        }
+      }          
+    });
+  });
 
-
+//UIrouter
+//scalibility and maintainbility issue
 //routing!=router
+//no back
+//refresh all var gone
